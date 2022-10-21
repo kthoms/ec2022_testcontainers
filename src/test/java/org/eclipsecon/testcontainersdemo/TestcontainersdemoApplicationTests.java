21d1111ab99aa7b5ab9bee93541ab7d012e3009b
Karsten Thoms <thoms@itemis.de>
2022-10-21 18:57:54 +0200

    Add DynamicPropertySource

--- src/test/java/org/eclipsecon/testcontainersdemo/TestcontainersdemoApplicationTests.java
+++ src/test/java/org/eclipsecon/testcontainersdemo/TestcontainersdemoApplicationTests.java
@@ -3,9 +3,13 @@ package org.eclipsecon.testcontainersdemo;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
 import org.testcontainers.containers.PostgreSQLContainer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
@@ -18,8 +22,10 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 @SpringBootTest
 @Testcontainers
 class TestcontainersdemoApplicationTests {
+    private static final Logger LOG = LoggerFactory.getLogger(TestcontainersdemoApplicationTests.class);
+
     @Container
-    PostgreSQLContainer postgreSQLContainer = new PostgreSQLContainer("postgres:latest")
+    static PostgreSQLContainer container = new PostgreSQLContainer("postgres:latest")
             .withUsername("demo")
             .withPassword("supersecret")
             .withDatabaseName("test");
@@ -33,6 +39,16 @@ class TestcontainersdemoApplicationTests {
     @BeforeEach
     void setUp() {
         repository.deleteAll();
+
+        LOG.info("Postgres Test DB URL: {}", container.getJdbcUrl());
+    }
+
+    // since SpringBoot 2.2.6
+    @DynamicPropertySource
+    static void properties (DynamicPropertyRegistry registry) {
+        registry.add("spring.datasource.url", container::getJdbcUrl);
+        registry.add("spring.datasource.username", container::getUsername);
+        registry.add("spring.datasource.password", container::getPassword);
     }
 
     @Test
