14a24f3f672c1caa217d7900fc877e755634a27c
Karsten Thoms <thoms@itemis.de>
2022-10-21 12:32:57 +0200

    Initial

---
+++ src/main/java/org/eclipsecon/testcontainersdemo/TestcontainersdemoApplication.java
@@ -0,0 +1,13 @@
+package org.eclipsecon.testcontainersdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestcontainersdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestcontainersdemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/eclipsecon/testcontainersdemo/TestcontainersdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package org.eclipsecon.testcontainersdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestcontainersdemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
