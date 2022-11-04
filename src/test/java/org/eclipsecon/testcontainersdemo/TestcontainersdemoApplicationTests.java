d60575f01c9d07666bb22ee4cf4f8468682b6b9c
Karsten Thoms <thoms@itemis.de>
2022-11-04 09:22:27 +0100

    rename Photo -> Music

--- src/test/java/org/eclipsecon/testcontainersdemo/TestcontainersdemoApplicationTests.java
+++ src/test/java/org/eclipsecon/testcontainersdemo/TestcontainersdemoApplicationTests.java
@@ -1,13 +1,10 @@
 package org.eclipsecon.testcontainersdemo;
 
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.dao.DataIntegrityViolationException;
-
-import java.sql.SQLException;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
@@ -15,10 +12,10 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 @SpringBootTest
 class TestcontainersdemoApplicationTests {
     @Autowired
-    PhotoAlbumRepository repository;
+    MusicAlbumRepository repository;
 
     @Autowired
-    PhotoService service;
+    MusicService service;
 
     @BeforeEach
     void setUp() {
@@ -31,7 +28,7 @@ class TestcontainersdemoApplicationTests {
 		assertThat(repository.count()).isEqualTo(0);
 
         // WHEN
-        PhotoAlbum album = service.createAlbum("Memento Mori", "Depeche Mode");
+        MusicAlbum album = service.createAlbum("Memento Mori", "Depeche Mode");
 
         // THEN
         assertThat(album.getId()).isNotNull();
